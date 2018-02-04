d967139084ddefecc4cf72aa520a1715a1eae2ca
walalal <2115451606@qq.com>
2018-02-04 16:56:31 +0800

    edit meandown.js

--- meadowlark.js
+++ meadowlark.js
@@ -8,14 +8,7 @@ app.set('view engine','handlebars');
 
 app.set('port',process.env.PORT||3000);
 
-var fortunes = [
-     "Conquer your fears or they will conquer you.",
-     "Rivers need springs.",
-     "Do not fear what you don't know.",
-     "You will have a pleasant surprise.",
-     "Whenever possible,keep it simple.",
-];
-
+var fortune = require('./lib/fortune.js');
 
 app.use(express.static(__dirname+'/public'));
 app.get('/',function(req,res){
@@ -24,9 +17,7 @@ app.get('/',function(req,res){
 
 app.use(express.static(__dirname+'/public'));
 app.get('/about',function(req,res){
-     var randomFortune=
-         fortunes[Math.floor(Math.random()*fortunes.length)];
-     res.render('about',{fortune:randomFortune});
+     res.render('about',{fortune:fortune.getFortune()});
 });
 
 app.use(express.static(__dirname+'/public'));
